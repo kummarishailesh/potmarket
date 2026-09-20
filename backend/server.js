04c3549b45e119dd2e1be73849575fdf6942a48f
Kummari_Shailesh_Kumar <24p61a6697@vbithyd.ac.in>
2026-09-20 12:34:51 +0530

    Allow GitHub Pages API origin

--- backend/server.js
+++ backend/server.js
@@ -32,6 +32,7 @@ const allowedOrigins = [
     'http://localhost:3001',
     'http://127.0.0.1:3000',
     'http://127.0.0.1:3001',
+    'https://kummarishailesh.github.io',
     publicAppOrigin
 ].filter(Boolean);
 
